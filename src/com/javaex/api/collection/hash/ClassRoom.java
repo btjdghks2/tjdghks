8c3c8be34f2a9ea9b35f69c4317e60cb714d145d
user <user@DESKTOP-MQU1QVN>
2021-05-11 10:19:00 +0900

    반복자. map

---
+++ src/com/javaex/api/collection/hash/ClassRoom.java
@@ -0,0 +1,45 @@
+package com.javaex.api.collection.hash;
+
+public class ClassRoom {
+	private String subject;
+	private String roomName;
+	
+	// 생성자
+	public ClassRoom(String subject) {
+		this.subject = subject;
+	}
+	
+	public ClassRoom(String subject,
+					 String roomName) {
+		this(subject);
+		this.roomName = roomName;
+	}
+
+
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public void setRoomName(String roomName) {
+		this.roomName = roomName;
+	}
+	// ContainsValue
+	// 객체의 동등성 비교
+
+	@Override
+	public int hashCode() {
+		// 
+		return subject.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// 
+		if (obj instanceof ClassRoom) {
+			ClassRoom other = (ClassRoom)obj;
+			subject.equals(objother.subject) &&
+			roomName.equals(other.roomName);
+		}
+		return super.equals(obj);
+	}
